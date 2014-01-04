86df1b8fad808b13ed923de8a4788188e6e7b67b
Wit Mesh <wmescheryakov@gmail.com>
2014-01-05 00:28:35 +0400

    Suggestion for schedule GUI

---
+++ Schedule/src/Form.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+
+public class Form extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	public Form() {
+		super("Shedule");
+	}
+
+	public static void main(String[] args) {
+		Form form = new Form();
+		form.setVisible(true);
+	}
+
+}
