842a0b4ac56b35dcecc1e5288115e5e6b6caabc0
Wit Mesh <wmescheryakov@gmail.com>
2014-01-08 02:25:30 +0400

    Added Search button

--- Schedule-Android/gen/com/example/schedule_android/R.java
+++ Schedule-Android/gen/com/example/schedule_android/R.java
@@ -21,10 +21,12 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_search=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_search=0x7f080001;
+        public static final int action_settings=0x7f080002;
         public static final int taskList=0x7f080000;
     }
     public static final class layout {
@@ -34,7 +36,8 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
+        public static final int action_search=0x7f050001;
+        public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
     }
     public static final class style {
